9b38fa66cecc15bc9812be1124b0c72fa17f6339
gabriel <gabriels.bezerra@gmail.com>
2026-07-04 10:01:44 -0300

    Novo projeto

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,18 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10!');
+
+function verificarChute() {
+console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
