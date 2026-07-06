bffe9385836884a953e3b4891cdb959388d0d01c
gabriel <gabriels.bezerra@gmail.com>
2026-07-06 20:07:22 -0300

    Refactor game logic and update restart button functionality

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,18 +1,52 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10!');
+function exibirMensagemInicial(){
+   exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10!'); 
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
-console.log(numeroSecreto);
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabéns! Você acertou o número secreto!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `você acertou em ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) { 
+            exibirTextoNaTela('p', 'O número secreto é menor que ' + chute);
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior que ' + chute);
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
 
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', 
+        true);
+}
